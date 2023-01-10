50143fa7c8adf0289b57019aa7dd7f431dc7ce7d
jaguilar <julio.aguilar@lania.edu.mx>
2023-01-10 15:01:00 -0600

    Proyecto creado con Initializr

---
+++ src/main/java/com/az/proyecto/PrimerProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.az.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerProyectoApplication.class, args);
+	}
+
+}
