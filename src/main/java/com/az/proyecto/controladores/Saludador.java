767f3de8800299e1a9b0f1a070b4749798254592
jaguilar <julio.aguilar@lania.edu.mx>
2023-01-10 23:00:20 -0600

    Configuracion para usar thymeleaf

--- src/main/java/com/az/proyecto/controladores/Saludador.java
+++ src/main/java/com/az/proyecto/controladores/Saludador.java
@@ -1,6 +1,9 @@
 package com.az.proyecto.controladores;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,11 +12,14 @@ import java.time.LocalDate;
 @Controller
 public class Saludador {
 
+    final static Logger LOGGER = LoggerFactory.getLogger(Saludador.class);
     @RequestMapping("saludo")
-    public ModelAndView saludar() {
-        ModelAndView mav = new ModelAndView("bienvenida");
-        mav.addObject("mensaje", "Hola mundo desde Spring");
-        mav.addObject("fecha", LocalDate.now());
-        return mav;
+    public String saludar(Model modelo) {
+        LOGGER.debug("saludar()");
+
+        ModelAndView mav = new ModelAndView();
+        modelo.addAttribute("mensaje", "Hola mundo desde Spring");
+        modelo.addAttribute("fecha", LocalDate.now());
+        return "bienvenida";
     }
 }
---
+++ src/main/webapp/WEB-INF/bienvenida.jsp
@@ -0,0 +1,8 @@
+<html>
+    <head>
+    </head>
+    <body>
+    <h3>Mensaje desde el servidor:</h3>
+    <h2>${mensaje}</h2>
+    </body>
+</html>
