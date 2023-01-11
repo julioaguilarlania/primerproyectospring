e258be0148ef9d555c9a03a9e9596f80825455eb
jaguilar <julio.aguilar@lania.edu.mx>
2023-01-11 15:04:15 -0600

    Entidad Vehiculo

---
+++ src/main/java/com/az/proyecto/entidades/Vehiculo.java
@@ -0,0 +1,69 @@
+package com.az.proyecto.entidades;
+
+import java.time.LocalDateTime;
+
+public class Vehiculo {
+    private String placas;
+    private String color;
+    private String marca;
+    private String modelo;
+    private Double kilometraje;
+    private LocalDateTime fechaIngreso;
+    private LocalDateTime fechaSalida;
+
+    public String getPlacas() {
+        return placas;
+    }
+
+    public void setPlacas(String placas) {
+        this.placas = placas;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Double getKilometraje() {
+        return kilometraje;
+    }
+
+    public void setKilometraje(Double kilometraje) {
+        this.kilometraje = kilometraje;
+    }
+
+    public LocalDateTime getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(LocalDateTime fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public LocalDateTime getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(LocalDateTime fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+}
--- src/main/webapp/WEB-INF/bienvenida.jsp
+++ src/main/webapp/WEB-INF/bienvenida.jsp
@@ -4,5 +4,6 @@
     <body>
     <h3>Mensaje desde el servidor:</h3>
     <h2>${mensaje}</h2>
+    <div>${fecha}</div>
     </body>
 </html>
