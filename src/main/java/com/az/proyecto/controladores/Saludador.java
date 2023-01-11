25d3dc656e921b1e8ac3db548276721ee50a9f47
jaguilar <julio.aguilar@lania.edu.mx>
2023-01-10 23:29:42 -0600

    Dependencias y configuracion correctas para JSP

--- src/main/java/com/az/proyecto/controladores/Saludador.java
+++ src/main/java/com/az/proyecto/controladores/Saludador.java
@@ -1,5 +1,7 @@
 package com.az.proyecto.controladores;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,8 +11,10 @@ import java.time.LocalDate;
 @Controller
 public class Saludador {
 
+    final static Logger LOGGER = LoggerFactory.getLogger(Saludador.class);
     @RequestMapping("saludo")
     public ModelAndView saludar() {
+        LOGGER.debug("saludar()");
         ModelAndView mav = new ModelAndView("bienvenida");
         mav.addObject("mensaje", "Hola mundo desde Spring");
         mav.addObject("fecha", LocalDate.now());
