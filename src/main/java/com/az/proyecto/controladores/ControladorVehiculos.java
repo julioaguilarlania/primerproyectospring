1a45cdf67efaa8f5a94a3bf70372f79e67575848
jaguilar <julio.aguilar@lania.edu.mx>
2023-01-12 11:59:35 -0600

    Busqueda de Vehiculo

--- src/main/java/com/az/proyecto/controladores/ControladorVehiculos.java
+++ src/main/java/com/az/proyecto/controladores/ControladorVehiculos.java
@@ -1,9 +1,11 @@
 package com.az.proyecto.controladores;
 
+import com.az.proyecto.entidades.BaseVehiculos;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -18,9 +20,11 @@ public class ControladorVehiculos {
     }
 
     @RequestMapping(value = "buscar", params = "placas")
-    public ModelAndView buscarVehiculo() {
+    public ModelAndView buscarVehiculo(@RequestParam("placas") String p) {
         LOGGER.debug("buscarVehiculo()");
         ModelAndView mav = new ModelAndView("resultados");
+        BaseVehiculos base = new BaseVehiculos();
+        mav.addObject("vehiculo", base.getVehiculoPorPlacas(p));
         return mav;
     }
 }
